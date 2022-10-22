f64c43b23cb9e2d594c827e13122bb0ed564745f
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2022-10-22 23:37:49 +0900

    바닐라JS #3.0~3.2 document Object, Searching For Elements

---
+++ 바닐라JS/searchElement.js
@@ -0,0 +1,11 @@
+const one = document.getElementById("one");
+// return 1 element
+const two = document.getElementsByClassName("two");
+// return array
+const three = document.getElementsByTagName("three");
+// return array
+const four = document.querySelector("#one .two three");
+// return 1 element
+// super cool
+const five = document.querySelectorAll("#one .two three");
+// return array
